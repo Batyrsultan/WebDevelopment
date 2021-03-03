343f05534f1089048def5d72b1be2dc6ce86c71e
Batyrsultan <78595913+Batyrsultan@users.noreply.github.com>
2021-03-03 16:23:13 +0600

    5st commit

---
+++ week5/MyFirstProject/src/app/products.ts
@@ -0,0 +1,131 @@
+export const products = [
+  {
+    name: 'Amazon Basics Classic Puresoft PU-Padded Mid-Back Office Computer Desk Chair with Armrest - Black',
+    price: 78.99,
+    // tslint:disable-next-line:max-line-length
+    description: 'Comfortable office chair upholstered in durable polyurethane bonded leather Padded seat and back for all-day comfort',
+    rating: 4.9,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71i08qnZeDL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/712Y-X3h6WL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71RCA2errrL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/AmazonBasics-Puresoft-PU-Padded-Mid-Back-Computer/dp/B081H3Y5NW/ref=sr_1_6?dchild=1&keywords=amazonbasics&pd_rd_r=5038d93b-b542-4e03-9dac-c5756110e172&pd_rd_w=PpYCi&pd_rd_wg=XiMrP&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=86B5S8VX5FM8EWTGV6HK&qid=1614323969&sr=8-6'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Fitbit Charge 4 Fitness and Activity Tracker with Built-in GPS, Heart Rate, Sleep & Swim Tracking, Black/Black, One Size (S &L Bands Included)',
+    price: 133.78,
+    // tslint:disable-next-line:max-line-length
+    description: 'Use built-in GPS to see your pace and distance on screen during outdoor runs, rides, hikes and more and see a workout intensity map in the app that shows your heart rate changes along your route',
+    rating: 4.8,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71B-KUciynL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/61SJeFZZWqL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71smqRr0pmL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Fitbit-Fitness-Activity-Tracking-Included/dp/B084CQ41M2/ref=sr_1_8?dchild=1&fst=as%3Aoff&pf_rd_i=16225014011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a3460e00-9eac-4cab-9814-093998a3f6d8&pf_rd_r=EFVDXN5YFTEARPAEW9A5&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1489014608&rnid=16225014011&s=sporting-goods&sr=1-8'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'YOSUDA Indoor Cycling Bike Stationary - Cycle Bike with Ipad Mount ＆Comfortable Seat Cushion (Gray)',
+    price: 336.99,
+    // tslint:disable-next-line:max-line-length
+    description: 'Smooth Stationery Bike】35lbs flywheel and heavy-duty steel frame of the exercise bike guarantee the stability while cycling. The belt driven system provides a smoother and quieter ride than chain transport. It wont disturb your apartment neighbors or sleeping kids',
+    rating: 4.8,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/81s5GF86cxL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81ssrhk9trL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/81zUjRrGZLL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/dp/B086LKSR2Z/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B086LKSR2Z&pd_rd_w=xX2AS&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=5Vkgk&pf_rd_r=TBK30GQY69HASZ7WVW4D&pd_rd_r=a03a9d7e-0d34-4476-a496-9fdccf8120fd&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNFpWUFY5WjhGQTlKJmVuY3J5cHRlZElkPUEwOTI0NDAzMVVQNzNFSlg5SFIzRiZlbmNyeXB0ZWRBZElkPUExMDQ1NTk0UEZKODZVSkNIMEFXJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Spalding NBA Street Outdoor Basketball',
+    price: 45.26,
+    // tslint:disable-next-line:max-line-length
+    description: 'Performance: Durable outdoor rubber cover Superior control: Deep channel design for easy handles',
+    rating: 4.4,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/91PlxEj64%2BL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/911EWfO-R8L._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/91vMT-aviXL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Spalding-NBA-Street-Basketball-Official/dp/B0009VELG4/ref=sr_1_24?dchild=1&fst=as%3Aoff&pf_rd_i=16225014011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a3460e00-9eac-4cab-9814-093998a3f6d8&pf_rd_r=EFVDXN5YFTEARPAEW9A5&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1489014608&rnid=16225014011&s=sporting-goods&sr=1-24'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Wrist Brace for Carpal Tunnel, Adjustable Wrist Support Brace with Splints Right Hand, Small/Medium, Arm Compression Hand Support for Injuries, Wrist Pain, Sprain, Sports',
+    price: 25.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Perfect Customer Service:If you are unhappy with your wrist brace, please do not hesitate to contact us. We value our customers and want each person to be 100% satisfied. You have nothing to lose but the pain youre suffering!',
+    rating: 4.3,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61qZTWJcsHL._AC_SL1490_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/718p1uaWzIL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/719CcueI3eL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Splints-Carpal-Adjustable-Relieve-Injuries/dp/B07BK4PXF7/ref=sr_1_47?dchild=1&fst=as%3Aoff&pf_rd_i=16225014011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a3460e00-9eac-4cab-9814-093998a3f6d8&pf_rd_r=EFVDXN5YFTEARPAEW9A5&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614340560&rnid=16225014011&s=sporting-goods&sr=1-47'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Spalding Single Action Plastic Championship Ball Pump',
+    price: 12.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Built-in needle storage Includes toy adapter 12 Inch barrel',
+    rating: 4.1,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71RIHeN-oRL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71RIHeN-oRL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/71RIHeN-oRL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Hydrating-Argan-Conditioner-Arvazallia-Damaged/dp/B00I32AN4K/ref=sr_1_18?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=NE1YZ166TCCQ59HXGC4J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-18'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Контролирует тетя Джеки 15 унций - Увлажняющий и смягчающий кондиционер "Anti-Poof"',
+    price: 6.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'ломкие волосы с натуральными экстрактами и протеинами, питают и глубоко вьются, трудно поддаются уходу',
+    rating: 4.5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/7167r9YjfYL._SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71qZpVbhCiL._SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61wFJiUonwL._SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Aunt-Jackies-control-15oz-Moisturizing/dp/B008R3GVN2/ref=pd_vtp_4?pd_rd_w=D9LZZ&pf_rd_p=55cbb45e-2534-4809-9135-12f41eecb852&pf_rd_r=DEMTCYMVA0FCWMWMVZ4Y&pd_rd_r=08b490fa-50d2-461e-9d06-6c56e74f329c&pd_rd_wg=jwML8&pd_rd_i=B008R3GVN2&psc=1'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Balaclava Ski Mask - Winter Face Mask for Men & Women - Cold Weather Gear for Skiing, Snowboarding & Motorcycle Riding Black',
+    price: 19.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Winter Ready: Winter has nothing on our Strike balaclava. Ready to brave all the elements from snow and sleet to wind, dust, cold, and UV, our ski mask isn’t letting in any unwanted intruders. So whether you blaze down the slopes like Shaun White, shovel snow or ATV in the desert like Mad Max, our balaclava’s got you covered.',
+    rating: 4.7,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/812TToi7RIL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81%2BACX0xMWL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/81Wl25FJgYL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/tgin-Green-Leave-Conditioner-Natural/dp/B00V73BZSU/ref=pd_vtp_2?pd_rd_w=VVipo&pf_rd_p=55cbb45e-2534-4809-9135-12f41eecb852&pf_rd_r=KDN9ER25RG768QB3FAVN&pd_rd_r=e93e850e-bb70-4151-b626-662086af7c8d&pd_rd_wg=aZmc2&pd_rd_i=B00V73BZSU&psc=1'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Маска для волос с маслом ши и несмываемый кондиционер для глубокого питания локонов и восстановления секущихся кончиков',
+    price: 6.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Маска для волос с маслом ши и несмываемый уход, Идеально подходит для волнистых и плотных локонов и среднего и сухого уровней влажности',
+    rating: 4.9,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61sJ0y2U8rL._SL1000_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/614A5FnASML._SL1000_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61UcW-iZNuL._SL1000_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Maui-Moisture-Hydrate-Butter-Ounce/dp/B01MXCO41W/ref=pd_vtp_14?pd_rd_w=VVipo&pf_rd_p=55cbb45e-2534-4809-9135-12f41eecb852&pf_rd_r=KDN9ER25RG768QB3FAVN&pd_rd_r=e93e850e-bb70-4151-b626-662086af7c8d&pd_rd_wg=aZmc2&pd_rd_i=B01MXCO41W&psc=1'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Monopoly Deal Card Game',
+    price: 7.99,
+    // tslint:disable-next-line:max-line-length
+    description: 'New Monopoly Deal card game that is moving through Family Game Nights everywhere',
+    rating: 4.8,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/91KOKqHaKpL._AC_SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/91KOKqHaKpL._AC_SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/91KOKqHaKpL._AC_SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Hasbro-B0965-Monopoly-Deal-Card/dp/B00NQQTZCO/ref=sr_1_7?dchild=1&fst=as%3Aoff&pf_rd_i=16225014011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a3460e00-9eac-4cab-9814-093998a3f6d8&pf_rd_r=EFVDXN5YFTEARPAEW9A5&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1489014608&rnid=16225014011&s=sporting-goods&sr=1-7'
+  }
+];
